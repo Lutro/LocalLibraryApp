ec867cc16a255961a10edf993ba649bccffb47c0
Lutro <stevengamez00@gmail.com>
2021-07-08 13:21:25 -0700

    skeleton

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+//Load express module
+const express = require("express");
+const app = express();
+const port = 8000;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+//listen for request on port 8000, and as a callback function have the port listened on logged
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
 });
